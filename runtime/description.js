de23eab6aa813cea9e3fcd4acf0a9cb27f152e4b
Douglas Stockwell <dstockwell@chromium.org>
2017-11-20 16:22:58 +1100

    Add platform assert

--- runtime/description.js
+++ runtime/description.js
@@ -9,7 +9,7 @@
  */
 "use strict";
 
-import assert from 'assert';
+import assert from '../platform/assert-web.js';
 import Type from './type.js';
 
 function getSuggestion(recipe, arc, relevance) {
